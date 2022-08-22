e4093561a6d2695b8665cd03495362977bf6919c
ArtTim <tim-murr@inbox.ru>
2022-08-23 00:43:36 +0300

    New project

---
+++ src/main/java/ru/pseudocode/springcourse/controller/HelloController.java
@@ -0,0 +1,13 @@
+package ru.pseudocode.springcourse.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello-world")
+    public String sayHello() {
+        return "hello-world";
+    }
+}
